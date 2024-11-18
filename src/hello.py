7759eb5ff3b200baa8eb5d1fd8e799e74c266934
asai <asai@fenrir.co.jp>
2024-11-18 13:09:34 +0000

    a

--- src/hello.py
+++ src/hello.py
@@ -1,15 +1,6 @@
 def main():
     print("Hello from workspace!")  # noqa: T201
 
-    a: str = 1
-
-    print(a)  # 型エラー用  # noqa: T201
-
-    print(1)  # リントエラー用
-
-
-    return 0  # フォーマット用
-
 
 if __name__ == "__main__":
     main()
