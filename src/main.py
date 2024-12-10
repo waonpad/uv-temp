faadbdeb1ab49fe0bf593db3093d0e5b1c92940d
waonpad <waonpad@gmail.com>
2024-12-10 20:13:49 +0900

    a

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import pymupdf.pro  # type: ignore  # noqa: PGH003
+import pymupdf.pro
 
-pymupdf.pro.unlock()  # type: ignore  # noqa: PGH003
+pymupdf.pro.unlock()
 
 test_docx = "test.docx"
 test_pptx = "test.pptx"
@@ -10,18 +10,18 @@ test_xlsx = "test.xlsx"
 def main():
     print("==== Testing docx ====")
     docx = pymupdf.open(test_docx)
-    docx_page = docx.load_page(0)  # type: ignore  # noqa: PGH003
-    print(docx_page.get_textpage().extractHTML())  # type: ignore  # noqa: PGH003
+    docx_page = docx.load_page(0)
+    print(docx_page.get_textpage().extractHTML())
 
     print("==== Testing pptx ====")
     pptx = pymupdf.open(test_pptx)
-    pptx_page = pptx.load_page(0)  # type: ignore  # noqa: PGH003
-    print(pptx_page.get_textpage().extractHTML())  # type: ignore  # noqa: PGH003
+    pptx_page = pptx.load_page(0)
+    print(pptx_page.get_textpage().extractHTML())
 
     print("==== Testing xlsx ====")
     xlsx = pymupdf.open(test_xlsx)
-    xlsx_page = xlsx.load_page(0)  # type: ignore  # noqa: PGH003
-    print(xlsx_page.get_textpage().extractHTML())  # type: ignore  # noqa: PGH003
+    xlsx_page = xlsx.load_page(0)
+    print(xlsx_page.get_textpage().extractHTML())
 
 
 if __name__ == "__main__":
